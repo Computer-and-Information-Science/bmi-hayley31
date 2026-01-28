d720af3909eb4294645e3f2c88c01e5b13a0dbdd
hayley31 <hayley.osconarvaez@student.northampton.edu>
2026-01-28 18:59:29 +0000

    Bmi works

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main() {
  
+int height, weight;
+height = 60;
+weight = 127;
 
+double kg, meters, bmi;
+
+kg = weight*0.453492;
+meters = height * 0.0254;
+
+bmi = kg/(meters*meters);
+
+cout << "The BMI for this person is: " << bmi << endl;
 
 }
